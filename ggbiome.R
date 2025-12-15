262ed412f90f837c749bb11a86bf1b69590649dd
Guofa-Su <73263097+Guofa-Su@users.noreply.github.com>
2025-12-15 12:57:45 +0800

    plot the biomes based on the mean annual precipitation and temperature of the ranges of spececies

---
+++ ggbiome.R
@@ -0,0 +1,7 @@
+##data analysis in ecology
+# install ggbiome
+if (!require(devtools)) install.packages("devtools")
+devtools::install_github("valentinitnelav/plotbiomes")
+library(ggplot2)
+library(ggbiomes)
+
